0de5545fd18ff18a80117bd0afb270b3c922bb13
strange051 <agrawalshashwat22@gmail.com>
2025-01-28 17:21:30 +0530

    changed folder structure

--- assgn_1_corona/virtualenv/Scripts/activate.bat
+++ assgn_1_corona/virtualenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Home\Desktop\OSTDS\py39
+set VIRTUAL_ENV=D:\Home\Desktop\git\OSTDS-Projects-Sem-6\assgn_1_corona\virtualenv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(py39) %PROMPT%
+set PROMPT=(virtualenv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
